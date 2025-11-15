ea0fdb12927283be767ebd603bbfcf3d344c76a3
Darylarz <darylariza10@gmail.com>
2025-11-15 01:13:15 -0400

    aaaaaaaaa 1:13am

--- database/seeders/InventoryTypeSeeder.php
+++ database/seeders/InventoryTypeSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\InventoryType;
 
-class InventoryTypesSeeder extends Seeder
+class InventoryTypeSeeder extends Seeder
 {
     public function run()
     {
